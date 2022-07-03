5f13ca6fc484dd69c2fd62d7978f9e646fa267a5
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-07-03 13:27:16 -0300

    change editSchema for adequate the date

--- src/schemas/editRegistersSchema.js
+++ src/schemas/editRegistersSchema.js
@@ -2,7 +2,8 @@ import joi from 'joi';
 
 const editRegistersSchema = joi.object({
   value: joi.number().required(),
-  description: joi.string().trim().required()
+  description: joi.string().trim().required(),
+  date: joi.date().required()
 });
 
 export default editRegistersSchema;
